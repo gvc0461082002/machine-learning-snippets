e000307d46e995f89856f9d50938c335618d42db
lulu <4156178@qq.com>
2018-10-14 23:53:14 +1100

    bag of words

---
+++ guide_encode_categorical_calues_in_python.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+# Define the headers since the data does not have any
+headers = ["symboling", "normalized_losses", "make", "fuel_type", "aspiration",
+           "num_doors", "body_style", "drive_wheels", "engine_location",
+           "wheel_base", "length", "width", "height", "curb_weight",
+           "engine_type", "num_cylinders", "engine_size", "fuel_system",
+           "bore", "stroke", "compression_ratio", "horsepower", "peak_rpm",
+           "city_mpg", "highway_mpg", "price"]
+
+# Read in the CSV file and convert "?" to NaN
+df = pd.read_csv("http://mlr.cs.umass.edu/ml/machine-learning-databases/autos/imports-85.data",
+                  header=None, names=headers, na_values="?" )
+print(df.head())
+print(df.dtypes)
+
+obj_df = df.select_dtypes(include=['object']).copy()
+print(obj_df.head())
+
+print(obj_df[obj_df.isnull().any(axis=1)])
+
+obj_df["num_doors"].value_counts()
+
+obj_df = obj_df.fillna({"num_doors": "four"})
+
+cleanup_nums = {"num_doors":     {"four": 4, "two": 2},
+                "num_cylinders": {"four": 4, "six": 6, "five": 5, "eight": 8,
+                                  "two": 2, "twelve": 12, "three":3 }}
+
+obj_df.replace(cleanup_nums, inplace=True)
+obj_df.head()
